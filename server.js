791073fa52293fd136c921287f3e899469b41f68
Prabhat Bahukhandi <prabhat.bahukhandi@myntra.com>
2019-06-28 12:01:42 +0530

    Express Server initialisation: sending the html file

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const DIST_DIR = __dirname;
+const HTML_FILE = path.join(DIST_DIR, "index.html");
+
+app.use(express.static(DIST_DIR));
+app.get("*", (req, res) => {
+  res.send(HTML_FILE);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT: ${PORT}`);
+});
